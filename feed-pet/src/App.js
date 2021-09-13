eedc0e76f59373c51e22b10ecc1d2366ebca9e33
lucas jose roncaglio <lucas.roncaglio1710@gmail.com>
2021-09-13 17:13:26 -0300

    Desenvolvimento NavBar - Lucas

--- feed-pet/src/App.js
+++ feed-pet/src/App.js
@@ -1,12 +1,36 @@
+
 import './App.css';
-import { Ola } from './components/teste';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/home';
+import About from './components/pages/about';
+import Events from './components/pages/events';
+import AnnualReport from './components/pages/annual';
+import Teams from './components/pages/team';
+import Blogs from './components/pages/blogs';
+import SignUp from './components/pages/signup';
+
 
 function App() {
-  return (
-    <div className="App">
-      <Ola nome="Teste"></Ola>
-    </div>
-  );
+return (
+	<Router>
+	<Navbar />
+	<Switch>
+		<Route path='/' exact component={Home} />    
+		<Route path='/about' component={About} />
+		<Route path='/events' component={Events} />
+		<Route path='/annual' component={AnnualReport} />
+		<Route path='/team' component={Teams} />
+		<Route path='/blogs' component={Blogs} />
+		<Route path='/sign-up' component={SignUp} />
+	</Switch>
+	</Router>
+);
 }
 
 export default App;
+
+
+  
+
+
