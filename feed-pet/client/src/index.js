21c2b3f374af6c50a0559b4a5783906fbee96e11
lucas jose roncaglio <lucas.roncaglio1710@gmail.com>
2021-09-21 15:48:48 -0300

    implementando login

--- feed-pet/client/src/index.js
+++ feed-pet/client/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import "./index.css";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-<React.StrictMode>
-	<App />
-</React.StrictMode>,
-document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
+
