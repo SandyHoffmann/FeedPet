922cac665f8c29f57002b1db34b65bf359a5ba8e
Sandy Hoffmann <manglesan98@gmail.com>
2021-09-21 00:58:54 -0300

    Pesquisa Funcional - Sandy

--- feed-pet/client/src/components/ComponentsReact/Home/Tabela/index.js
+++ feed-pet/client/src/components/ComponentsReact/Home/Tabela/index.js
@@ -10,42 +10,69 @@ export function FiltragemHome(props){
     const [filtrarTexto, setFiltrarTexto] = useState("")
     const [card, setCard] = useState([])
     const [cardFiltrada,setCardFiltrada]=useState([])
+    const [filtros,setFiltros]=useState([])
 
     useEffect(async () => {
         try {
             const res = await api.get(`/animais`);
             const informacao = res.data;
             setCard(informacao.reverse())
+            setFiltros(
+                {raca:"",tipo_animal:"",status:"",cor:""}
+                )
             setCardFiltrada(informacao.reverse())
         } catch (error) {
             console.log(error)
         }
     }, [])
 
-    function filtrar(informacao){
+    function filtrar(informacao=filtrarTexto){
         let lista_filtrada = []
+        let lista
+        let lista_filtros
+        let verificar
+
         for (let i of card){
-            if (i.nome.includes(informacao)){
-                lista_filtrada.push(i)
+            lista = Object.values(i)
+            lista_filtros = Object.values(filtros)
+            verificar = true
+            for (let e of lista_filtros){
+                console.log(e)
+                if (!lista.includes(e)&&e!=""){
+                    console.log(lista)
+                    verificar=false
+                }
             }
+            if (i.nome.toLowerCase().includes(informacao.toLowerCase())||(informacao=="")){
+                console.log(informacao!="")
+                if (verificar) lista_filtrada.push(i) 
+
+            }          
         }
         setCardFiltrada(lista_filtrada)
     }
+
 
     function handleChange(e){
         setFiltrarTexto(e.target.value)
         filtrar(e.target.value)
     }
-    
+
     function handleClick(e){
-        setFiltrarTexto(e.target.value)
+        const dic = filtros
+        const value = e.target.value;
+        const nome = e.target.name;
+        dic[nome]=value
+        setFiltros(dic)
+        console.log(filtros)
+        filtrar()
     }
 
     
         return(
             <>
             <div className="Tabela">
-                <Pesquisa texto={filtrarTexto} onChange={handleChange} onClick={handleClick}/>
+                <Pesquisa texto={filtrarTexto} onChange={handleChange} onClick={handleClick} onChangeOption={handleClick} tipo_animal={filtros.tipo_animal}/>
                 <Cardhometeste animais={cardFiltrada} className="cardReact" />
             </div>
             </>
