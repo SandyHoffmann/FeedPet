91b1d4821d9d876c49375d3df9ddf6bbcc6b9457
Charles David Duggen <charles.duggen@alunos.sc.senac.br>
2021-09-22 17:26:53 -0300

    em execução

---
+++ feed-pet/client/src/components/CaixaMesagem/CaixaDeTexotLateralEsquerda.js
@@ -0,0 +1,23 @@
+export function CaiDeTextoLateralEsquerda(){
+    return(
+<div classeName = "texto">
+    <h1>Paragrafo</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ornare cursus nulla. Nulla vestibulum sollicitudin 
+        enim nec aliquam. Suspendisse pretium congue volutpat. Quisque vitae sollicitudin lorem. Morbi feugiat nisi ut lectus 
+        ultrices convallis. Nullam maximus, nunc ut viverra iaculis, justo enim dignissim ex, sed finibus augue dolor a magna. 
+        Cras lacus turpis, bibendum sit amet semper ut, cursus nec felis. Donec vitae porttitor enim. Nullam imperdiet, est ut imperdiet 
+        viverra, magna nulla vestibulum turpis, pulvinar commodo dolor erat sit amet nunc. Maecenas ultrices leo justo, eu dignissim dolor 
+        tincidunt nec. Phasellus sit amet ipsum fringilla, eleifend eros a, aliquet mi. Pellentesque velit sapien, venenatis quis porttitor nec, tincidunt a elit.
+
+Sed sed justo risus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse consectetur, odio 
+et efficitur venenatis, arcu lorem feugiat sapien, eu dapibus augue lectus eget est. Fusce pretium laoreet sem a ultrices. Mauris id urna 
+vel ipsum lobortis ultrices vel ut mi. Integer pharetra ut massa id vehicula. Pellentesque vitae dui ut velit blandit vestibulum tincidunt et risus.
+
+Morbi in odio enim. Integer eget mauris lacus. Fusce condimentum commodo velit, et porttitor massa tincidunt ut. Class aptent taciti 
+sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam quis ultrices libero. Nam aliquam justo ac molestie pulvinar.
+ Praesent in erat mattis, lacinia nisi at, porttitor nibh. Nulla nec neque ultrices, viverra nisl sit amet, finibus felis. Nam venenatis urna dolor,
+  quis laoreet odio maximus vel. Suspendisse rhoncus justo pellentesque, convallis leo eu, dignissim lacus.</p>
+</div>
+        
+    )
+}
