7b8674dffc6f57b534b546dad51bacbd4e09ac26
Sandy Hoffmann <manglesan98@gmail.com>
2021-09-24 15:09:09 -0300

    Login e Cadastro - Estilizado - Sandy

---
+++ feed-pet/client/src/components/ComponentsReact/FormCadastro/index-estilizado.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { api } from "../../../service";
+import { useState, useEffect } from "react";
+
+export function FormCadastroEstilizado(){
+    const [nome,setNome] = useState("")
+    const [email,setEmail] = useState("")
+    const [senha,setSenha] = useState("")
+    const [imagem,setImagem] = useState("")
+
+    async function handleChange(e){
+        const value = e.target.value;
+        const nome = e.target.name;
+        if (nome=="nome"){
+            setNome(value)
+        } else if (nome=="email"){
+            setEmail(value)
+        } else if (nome=="senha"){
+            setSenha(value)
+        } else if (nome=="avatar"){
+            setImagem(e.target.files[0])
+        }
+        console.log(imagem)
+    }
+    
+    async function handleSubmit(e){
+        try {
+            e.preventDefault();
+            const formData = new FormData(e.target);
+
+            const res = await api.post(`/usuarios/`, 
+                                formData, {
+                                    headers: {
+                                        "Content-Type": `multipart/form-data;boundary=${formData._boundary}`,
+                                        }
+                                });
+            console.log(res.data)
+            setNome("")
+            setEmail("")
+            setSenha("")   
+            setImagem("")   
+            window.location.replace("/login");    
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+        return (
+            <>
+            <div className="container caixa">
+                <form onSubmit={handleSubmit} className="caixaElemento" enctype="multipart/form-data"> 
+                <h1>Cadastre-se</h1>
+                <br/>
+                <div className="form-group">
+                    <label htmlFor="nome">Nome:</label>
+                    <input type="text" className="form-control" id="nome" name="nome" aria-describedby="Nome" value={nome} onChange={handleChange} placeholder="Nome"/>
+                </div>
+                <br/>
+                <div className="form-group">
+                    <label htmlFor="email">Email:</label>
+                    <input type="text" className="form-control" id="email" name="email" aria-describedby="Email" value={email} onChange={handleChange} placeholder="Email"/>
+                </div>
+                <br/>
+                <div className="form-group">
+                    <label htmlFor="senha">Senha:</label>
+                    <input type="text" className="form-control" id="senha" name="senha" aria-describedby="Senha" value={senha} onChange={handleChange} placeholder="Senha"/>
+                </div>
+                <input type="file" name="avatar" class="img" onChange={handleChange}/>
+                <br/>
+                <button type="submit" className="btn btn-primary">Enviar</button>
+                </form>   
+                <br/>
+                </div>         
+            </>
+        );
+
+}
---
+++ feed-pet/client/src/components/ComponentsReact/PaginaLogin/Login/index-estilizado.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { api } from "../../../../service";
+import authServices from "../../../../service/authServices";
+import {id,secret} from '../../../../varAmbiente'
+import { Form } from 'react-bootstrap';
+import "./styles.css"
+import { Link } from "react-router-dom";
+
+const jwt = require('jsonwebtoken');
+
+const initialState = {
+    email:"",
+    senha:""
+}
+
+export class FormLoginEstilizado extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email:"",
+            senha:""
+        };
+    }
+
+    
+    handleChange = e => {
+        const value = e.target.value;
+        const nome = e.target.name;
+        this.setState({
+            [nome]:value
+        });
+    }
+    
+    handleSubmit = async e => {
+        try {
+            e.preventDefault();
+            // let token = jwt.decode(localStorage.getItem("token"),secret).sub
+            // console.log(token)
+            await authServices.signIn(this.state.email,this.state.senha)
+            this.setState({...initialState})
+            window.location.replace("/");    
+
+        } catch (error) {
+            console.log(this.state)
+        }
+    }
+
+    render() {
+
+        return (
+            <>
+            <div className="container caixa">
+                <form onSubmit={this.handleSubmit} className="caixaElemento">
+                <h1>Login</h1>
+                <div className="form-group">
+                    <br/>
+                    <label htmlFor="email">Email:</label>
+                    <input type="text" className="form-control" id="email" name="email" aria-describedby="Email" value={this.state.email} onChange={this.handleChange} placeholder="Email"/>
+                </div>
+                <br/>
+
+                <div className="form-group">
+                    <label htmlFor="senha">Nome:</label>
+                    <input type="text" className="form-control" id="senha" name="senha" aria-describedby="Senha" value={this.state.senha} onChange={this.handleChange} placeholder="Senha"/>
+                </div>
+                <br/>
+
+                <button type="submit" className="btn btn-primary">Enviar</button>
+                </form> 
+                <br/>
+                <Link to="/cadastro" className="caixaElemento">Cadastre-se</Link>
+
+                </div>
+
+            </>
+
+        );
+    }
+}
