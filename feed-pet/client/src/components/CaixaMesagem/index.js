ec3bad0d81aa8e56265d0a1c7f873fca5352cc78
Charles David Duggen <charles.duggen@alunos.sc.senac.br>
2021-09-27 17:23:45 -0300

    em execução

--- feed-pet/client/src/components/CaixaMesagem/index.js
+++ feed-pet/client/src/components/CaixaMesagem/index.js
@@ -3,10 +3,37 @@ import { CaixaDeTextoLateralDireita } from "./CaixaDeTexoLateralDireita";
 import { NavBarLateral } from "./NavBarLateral";
 import "./styles.css";
 import { BiSearchAlt2 } from 'react-icons/bi';
+import { useEffect, useState } from "react";
 
 
 
 export function CaixaMensagem() {
+    const [layoutResponsivo, setLayoutResponsivo] = useState(false);
+
+    useEffect(() => {
+        function resizeWindow() {
+            console.log("executou")
+            const width = window.innerWidth
+            || document.documentElement.clientWidth
+            || document.body.clientWidth;
+
+            if (width < 680) {
+                setLayoutResponsivo(true)
+            } else {
+                setLayoutResponsivo(false);
+            }                      
+        }
+
+        window.addEventListener("resize", resizeWindow);
+        resizeWindow();
+
+        return () => window.removeEventListener("resize", resizeWindow);
+    }, []);
+
+    if (layoutResponsivo) {
+        return <p>Oi</p>
+    }
+
     return (
         <div className="container-caixa-mensagem">
             <Cabecalho />
