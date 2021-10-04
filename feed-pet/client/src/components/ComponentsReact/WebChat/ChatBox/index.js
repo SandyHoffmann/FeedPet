60ad13df6a507bd6a4fb2c18fe6fc566c28a9305
Sandy Hoffmann <manglesan98@gmail.com>
2021-10-04 02:13:46 -0300

    Mudando Filtro e Chat

--- feed-pet/client/src/components/ComponentsReact/WebChat/ChatBox/index.js
+++ feed-pet/client/src/components/ComponentsReact/WebChat/ChatBox/index.js
@@ -1,7 +1,20 @@
+import "./styles.css"
+
 export function ChatBox(props) {
-    return(
+    return (
         <div className="chatBox" onClick={props.onClick}>
-            <h1 id={props.chat.id}>{props.chat.nome}</h1>
+            <div className="container mt-1 d-flex">
+                <div className="card p-3">
+                    <div className="d-flex">
+                        <div className="d-flex image rounded" >
+                        </div>
+                        <div className="ml-3 w-100">
+                            <p className="mb-0 mt-0 pchat" id={props.chat.id}>{props.chat.nome}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
     )
 }
