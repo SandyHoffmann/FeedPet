69961c0aef3ef9b3254dc5cf365c013d5de28613
Sandy Hoffmann <manglesan98@gmail.com>
2021-10-11 13:56:52 -0300

    Mudando Chat - Sandy

--- feed-pet/server/src/routes/chatsRoutes.js
+++ feed-pet/server/src/routes/chatsRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const chatControllers = require("../controllers/chatControllers");
 const authentication = require("../middlewares/authMiddleware");
 
-router.get("/", authentication(["usuario", "admin"]), chatControllers.getChats);
+router.get("/", chatControllers.getChats);
 router.get("/:id_chat", authentication(["usuario", "admin"]), chatControllers.getMensagens);
 router.post("/", authentication(["usuario", "admin"]),chatControllers.createChat);
 router.post("/mensagem/:id_chat",authentication(["usuario", "admin"]),chatControllers.createMensagem);
