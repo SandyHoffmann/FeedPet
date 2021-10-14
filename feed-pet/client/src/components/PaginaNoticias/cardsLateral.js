a6589910e860df15524dae54698a6659aaf5d91e
Charles David Duggen <charles.duggen@alunos.sc.senac.br>
2021-10-14 17:07:31 -0300

    em execução

--- feed-pet/client/src/components/PaginaNoticias/cardsLateral.js
+++ feed-pet/client/src/components/PaginaNoticias/cardsLateral.js
@@ -5,10 +5,10 @@ import React from 'react';
 import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn } from 'mdb-react-ui-kit';
 import { max } from 'moment';
 
-export  function CardsLateral({ titulo, subtitulo, onClick }) {
+export  function CardsLateral({ titulo, subtitulo, imagem, onClick }) {
   return (
     <MDBCard className= "cardLateral" onClick={onClick}>
-      <MDBCardImage className ="imagemCard"   src='https://mdbcdn.b-cdn.net/img/new/standard/nature/184.jpg' position='top' alt='...' />
+      <MDBCardImage className ="imagemCard"   src={imagem} position='top'  />
       <MDBCardBody>
         <MDBCardTitle className = "titulo">{titulo}</MDBCardTitle>
         <MDBCardText className= "Texto">
