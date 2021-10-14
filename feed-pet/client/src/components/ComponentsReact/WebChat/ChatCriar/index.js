8d569f8b37eeb2140f4e06758d1b5bd020f2885e
Sandy Hoffmann <manglesan98@gmail.com>
2021-10-14 02:19:16 -0300

    Edicao + Verificacao formularios - Sandy

--- feed-pet/client/src/components/ComponentsReact/WebChat/ChatCriar/index.js
+++ feed-pet/client/src/components/ComponentsReact/WebChat/ChatCriar/index.js
@@ -3,9 +3,9 @@ import { Button, Modal } from 'react-bootstrap';
 import { api } from "../../../../service";
 import { ChatBoxEmail } from '../ChatBoxEmail';
 import Select from 'react-select';
-import {RiChatNewLine} from 'react-icons/ri';
+import { RiChatNewLine } from 'react-icons/ri';
 import { socket } from '../../../../service/chat';
-
+import { VerificarErros } from "../../../../errorHandling";
 import "./styles.css"
 
 export function ChatCriar(props) {
@@ -28,84 +28,105 @@ export function ChatCriar(props) {
 
     }, [])
 
-    function handleChange(e){
+    function handleChange(e) {
         setpessoasDoChat(e)
-        
+
     }
 
-    async function handleSubmit(){
-        const nomeChat = nome?nome:pessoasDoChat[0].nome
-        let verificar = true
-        console.log(pessoasDoChat[0].id)
-        if (!nome){
-            props.chats.filter(
-                chat => chat.usuario.length ==2 &&
-                chat.usuario.map(
-                    usuarioChat => usuarioChat.id == pessoasDoChat[0].id &&
+    async function handleSubmit() {
+        try {
+            const nomeChat = nome && pessoasDoChat.length<2 ? pessoasDoChat[0].nome : nome
+            let verificar = true
+            console.log(pessoasDoChat[0].id)
+            if (!nome) {
+                if (pessoasDoChat.length<2){
+                    console.log(props.chats)
+                    props.chats.filter(
+                        chat => chat.usuario.length === 2 &&
+                            chat.usuario.map(
+                                usuarioChat => usuarioChat.id == pessoasDoChat[0].id &&
                                     (verificar = false)
-                )
-            )
+                            )
+                    )
+                }
+            }
+            if (verificar) {
+                console.log(pessoasDoChat)
+                const usuarios = pessoasDoChat.map(pessoa => pessoa.id)
+                const send = await api.post("/chats/", {
+                    "nome": nomeChat,
+                    descricao,
+                    usuarios
+                })
+                let chat = send.data
+                chat = Object.assign(chat, { ultimaMsg: { conteudo: "Nenhuma Mensagem" } })
+                let chats = props.chats
+                props.setchats(chats => { return [chat, ...chats] })
+                socket.emit("add chat", chat, usuarios)
+                handleClose()
+            } else {
+                let erro = { 'errors': [{ 'msg': "Você já tem uma conversa com este usuário!", 'param': 'chat' }] }
+                VerificarErros(erro)
+            }
+        } catch (error) {
+            console.log(error)
+            if (pessoasDoChat.length === 0) {
+                let erro = { 'errors': [{ 'msg': "Escolha algum usuário!", 'param': 'chat' }] }
+                VerificarErros(erro)
+            } if (error.response) {
+                let erros = error.response.data
+                console.log(erros)
+                VerificarErros(erros)
+            }
         }
-        if (verificar){
-            console.log(pessoasDoChat)
-            const usuarios=pessoasDoChat.map(pessoa => pessoa.id)
-            const send = await api.post("/chats/",{
-                "nome":nomeChat,
-                descricao,
-                usuarios
-            })
-            let chat = send.data
-            chat = Object.assign(chat,{ultimaMsg:{conteudo: "Nenhuma Mensagem"}})
-            let chats = props.chats
-            props.setchats(chats => {return [chat,...chats]})
-            socket.emit("add chat",chat,usuarios)
-            handleClose()
-        } else{
-            alert("Você ja tem uma conversa com este usuario!")
-            handleClose()
-        }
+
     }
 
     return (
         <>
-        <Button variant="light" onClick={handleShow}>
-            <RiChatNewLine></RiChatNewLine>
-        </Button>
+            <Button variant="light" onClick={handleShow}>
+                <RiChatNewLine></RiChatNewLine>
+            </Button>
 
-        <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>Criar Conversa</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <div className="chat__email__input"> 
-                <Select
-                        isMulti
-                        name="colors"
-                        options={pessoas?.map(pessoa => ({'id':pessoa.id,'value':pessoa.id,'nome':pessoa.nome,'email':pessoa.email,'label':pessoa.email}))}
-                        className="basic-multi-select"
-                        classNamePrefix="select"
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className="chat__email__box">
-                    <div className="chat__email__box__inputs">
-                        {(pessoasDoChat.length>1)&&
-                        <>
-                        <label htmlFor="nome">
-                            Nome do Grupo:
-                        </label>
-                        <input type="text" name="nome" value={nome} onChange={e => setNome(e.target.value)}></input>
-                        <label>
-                            Descricao do Grupo:
-                        </label>
-                        <input type="text" name="descricao" value={descricao} onChange={e => setDescricao(e.target.value)}></input>
-                        </>
-                        }
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Criar Conversa</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="chat__email__input">
+                        <Select
+                            isMulti
+                            name="colors"
+                            options={pessoas?.map(pessoa => ({ 'id': pessoa.id, 'value': pessoa.id, 'nome': pessoa.nome, 'email': pessoa.email, 'label': pessoa.email }))}
+                            className="basic-multi-select"
+                            classNamePrefix="select"
+                            onChange={handleChange}
+                        />
                     </div>
-                    <button onClick={handleSubmit}>Enviar</button>
-                </div>
-            </Modal.Body>
-        </Modal>
+                    <div className="chat__email__box">
+                        <div className="chat__email__box__inputs">
+                            {(pessoasDoChat.length > 1) &&
+                                <>
+                                    <div className="form-err nome-err">
+                                    <label htmlFor="nome">
+                                        Nome do Grupo:
+                                    </label>
+                                        <input type="text" name="nome" value={nome} onChange={e => setNome(e.target.value)}></input>
+                                    </div>
+                                    <div className="form-err descricao-err">
+                                        <label>
+                                            Descricao do Grupo:
+                                        </label>
+                                        <input type="text" name="descricao" value={descricao} onChange={e => setDescricao(e.target.value)}></input>
+                                    </div>
+                                </>
+                            }
+                        </div>
+                        <div className="form-err chat-err"></div>
+                        <button onClick={handleSubmit}>Enviar</button>
+                    </div>
+                </Modal.Body>
+            </Modal>
         </>
     )
 }
